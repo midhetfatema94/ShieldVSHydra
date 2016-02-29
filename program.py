c6ad9d74f3e247145081880ffc441b1db4d85a8f
Midhet Sulemani <midhetfatema94@gmail.com>
2016-02-29 13:27:04 +0530

    Resolved conflicts and pushed

--- program.py
+++ program.py
@@ -1,4 +1,17 @@
-def findInfiltrator(sender, rec1, rec2, rec3):
+def findShieldMembers(sender, rec1, rec2, rec3):
 	notInfiltrator = new Array()
 	if(sender == 'Nick Fury'):
-		notInfiltrator.push(rec1, rec2, rec3);
+		notInfiltrator.append(sender, rec1, rec2, rec3);
+
+	for i in notInfiltrator:
+		if(sender == i):
+			notInfiltrator.append(rec1, rec2, rec3)
+
+def addAllMembers(m1, m2, m3, m4):
+	allMembers.append(m1, m2, m3, m4)
+
+def findInfiltrators(shield, all):
+	return list(set(all) - set(shield))
+
+def main():
+	print "Welcome to Shield Vs. Hydra Algorithm"
